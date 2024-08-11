c70d7f5162a72d24126226f374b61d9e81c5956a
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-11 10:59:48 +0430

    Create User entity

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Bio { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
