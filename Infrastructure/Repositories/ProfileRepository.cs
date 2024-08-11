bb78cdb5619605c820341f4a240a2a2bf8653e87
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-11 18:01:23 +0430

    Create ProfileRepository

---
+++ Infrastructure/Repositories/ProfileRepository.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Repositories;
+
+namespace Infrastructure.Repositories
+{
+    public class ProfileRepository : IProfileRepository
+    {
+        public Task<string> GetBio(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetDateOfBirth(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetEmail(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetFirstName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetImageUrl(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetLastName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPhoneNumber(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateBio(int id, string bio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDateOfBirth(int id, DateTime dateOfBirth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmail(int id, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateFirstName(int id, string firstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateImageUrl(int id, string imageUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateLastName(int id, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePhoneNumber(int id, int phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
