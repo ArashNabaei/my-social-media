338c01d29cf226bd03029d192d64effe94c32388
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-11 11:08:36 +0430

    Create Post object

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Domain.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Caption { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+    }
+}
