8ab5330502b0c4b69c99d11520f93dea527afd4d
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-11 11:43:03 +0430

    Create IProfileRepository interface and add some methods inside that

---
+++ Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Domain.Repositories
+{
+    public interface IProfileRepository
+    {
+        Task<string> GetFirstName(int id);
+
+        Task<string> GetLastName(int id);
+
+        Task<string> GetBio(int id);
+
+        Task<string> GetEmail(int id);
+
+        Task<string> GetPhoneNumber(int id);
+
+        Task<string> GetImageUrl(int id);
+
+        Task<string> GetDateOfBirth(int id);
+
+        Task UpdateFirstName(int id, string firstName);
+
+        Task UpdateLastName(int id, string lastName);
+
+        Task UpdateBio(int id, string bio);
+
+        Task UpdateEmail(int id, string email);
+
+        Task UpdatePhoneNumber(int id, int phoneNumber);
+
+        Task UpdateImageUrl(int id, string imageUrl);
+
+        Task UpdateDateOfBirth(int id, DateTime dateOfBirth);
+
+    }
+}
