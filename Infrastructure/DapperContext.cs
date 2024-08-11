79942c004077830a66f5767da4329af389f260e4
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-11 17:01:04 +0430

    Create file DapperContext to handle database connection using dapper

---
+++ Infrastructure/DapperContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Infrastructure
+{
+    public class DapperContext : IDisposable
+    {
+
+        public IDbConnection Connection { get; }
+
+        private readonly IConfiguration _configuration;
+
+        private bool disposed;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            disposed = false;
+            _configuration = configuration;
+            Connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            Connection.Open();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                    Connection.Dispose();
+            }
+
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
