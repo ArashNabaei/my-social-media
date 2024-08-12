a7b914ec9de5818c1b0f61f439fa4c6ecaa6739c
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-12 16:36:46 +0430

    Add Username and Password to User object

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -9,6 +9,10 @@ namespace Domain.Entities
 
         public string LastName { get; set; }
 
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
         public string Bio { get; set; }
 
         public string Email { get; set; }
