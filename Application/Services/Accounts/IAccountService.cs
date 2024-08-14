a15a3655226c3c8cc4b2b5b7a00f6d8748c67e19
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-14 11:12:25 +0330

    Create Dto directory and UserDto object Create IAccountService

---
+++ Application/Services/Accounts/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using Domain.Entities;
+
+namespace Application.Services.Accounts
+{
+    public interface IAccountService
+    {
+
+        Task<IEnumerable<User>> GetAllUsers();
+
+        Task CreateUser(UserDto user);
+    }
+}
