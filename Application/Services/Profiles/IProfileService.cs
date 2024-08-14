aa36159949a6ebf37405d80f2851788e7dad1322
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-14 11:31:47 +0330

    Create IProfileService and add required methods

---
+++ Application/Services/Profiles/IProfileService.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Application.Services.Profiles
+{
+    public interface IProfileService
+    {
+
+        Task<string> GetBio(int id);
+
+        Task<DateTime> GetDateOfBirth(int id);
+
+        Task<string> GetEmail(int id);
+
+        Task<string> GetFirstName(int id);
+
+        Task<string> GetLastName(int id);
+
+        Task<string> GetImageUrl(int id);
+
+        Task<string> GetPhoneNumber(int id);
+
+        Task UpdateBio(int id, string bio);
+
+        Task UpdateDateOfBirth(int id, DateTime dateOfBirth);
+
+        Task UpdateEmail(int id, string email);
+
+        Task UpdateFirstName(int id, string firstName);
+
+        Task UpdateImageUrl(int id, string imageUrl);
+
+        Task UpdatePhoneNumber(int id, string phoneNumber);
+
+    }
+}
