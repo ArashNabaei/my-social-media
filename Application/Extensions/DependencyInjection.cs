152e77c4f3661e1d97c4c8f0fc9b4270dd58762c
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-15 18:08:59 +0330

    Create an extension for DependencyInjection in Application project

---
+++ Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Application.Services.Accounts;
+using Application.Services.Profiles;
+using Domain.Repositories;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IProfileService, ProfileService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {   
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IProfileRepository, ProfileRepository>();
+
+            services.AddScoped<DapperContext>();
+
+            return services;
+        }
+    }
+}
