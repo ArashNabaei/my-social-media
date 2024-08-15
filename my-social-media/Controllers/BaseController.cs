bb9b4e9a303cd52f09450bd422b97f89571529bb
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-15 10:50:17 +0330

    Create BaseController class as parent of controllers

---
+++ my-social-media/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_social_media.Controllers
+{
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        protected void SetAuthorizationHeader()
+        {
+            if (Request.Headers.ContainsKey("Authorization"))
+            {
+                var token = Request.Headers["Authorization"].ToString();
+                HttpContext.Request.Headers["Authorization"] = token;
+            }
+        }
+    }
+}
