49fc173b61deef5a71bfcb2c4ab500430d8939c9
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-17 11:11:56 +0330

    Refactor GetUser method in IAccountRepository

--- Domain/Repositories/IAccountRepository.cs
+++ Domain/Repositories/IAccountRepository.cs
@@ -8,6 +8,6 @@ namespace Domain.Repositories
 
         Task CreateUser(string username, string password);
 
-        Task<User> GetUserByUsername(string username);
+        Task<User> GetUserByUsernameAndPassword(string username, string password);
     }
 }
