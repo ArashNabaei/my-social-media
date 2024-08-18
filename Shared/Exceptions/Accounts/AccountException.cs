95eee6549d63df38018f19ed89b238a939f37eef
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-18 11:43:02 +0330

    Create AccountException class Add UserAlreadyExists Add UserNotFound

---
+++ Shared/Exceptions/Accounts/AccountException.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Shared.Exceptions.Accounts
+{
+    public class AccountException : BaseException
+    {
+        public AccountException(int code, string message) : base(code, message)
+        {
+        }
+
+        public static AccountException UserAlreadyExists()
+        {
+            return new AccountException(1001, "User already exists.");
+        }
+
+        public static AccountException UserNotFound()
+        {
+            return new AccountException(1002, "User was not found.");
+        }
+
+    }
+}
