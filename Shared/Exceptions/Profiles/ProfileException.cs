928e3470012367445e98cce9f3d9c5955f1b9977
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-18 18:28:32 +0330

    Create a custom exception for as ProfileException

---
+++ Shared/Exceptions/Profiles/ProfileException.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Shared.Exceptions.Profiles
+{
+    public class ProfileException : BaseException
+    {
+
+        public ProfileException(int code, string message) : base(code, message)
+        {
+            
+        }
+
+        public static ProfileException ProfileNotFound()
+        {
+            return new ProfileException(2001, "Profile was not found.");
+        }
+
+        public static ProfileException InvalidProfileData()
+        {
+            return new ProfileException(2002, "Invalid data.");
+        }
+
+    }
+}
