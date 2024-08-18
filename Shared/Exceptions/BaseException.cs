a87988dd2cd1fea8b66dadafe438040f235fd74d
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-18 11:25:46 +0330

    Create BaseException class as parent of exceptions

---
+++ Shared/Exceptions/BaseException.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Shared.Exceptions
+{
+    public abstract class BaseException : Exception
+    {
+
+        public int Code { get; set; }
+
+        protected BaseException(int code, string message) : base(message)
+        {
+            Code = code;
+        }
+
+    }
+}
