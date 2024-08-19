678a339374ea86e304fd2ed11f9081a71acf366c
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-19 12:36:32 +0330

    Implement UpdatePost method in Postrepository

--- Infrastructure/Repositories/PostRepository.cs
+++ Infrastructure/Repositories/PostRepository.cs
@@ -74,5 +74,22 @@ namespace Infrastructure.Repositories
             await _dapperContext.Connection.ExecuteAsync(query, parameters);
         }
 
+        public async Task UpdatePost(int userId, int postId, Post post)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("postId", postId);
+            parameters.Add("caption", post.Caption);
+            parameters.Add("imageUrl", post.ImageUrl);
+            parameters.Add("creationTime", post.CreationTime);
+            parameters.Add("userId", userId);
+
+            var query = "UPDATE Posts " +
+                "SET Caption = @caption, ImageUrl = @imageUrl, " +
+                "CreationTime = @creationTime " +
+                "WHERE Id = @postId AND UserId = @userId";
+
+            await _dapperContext.Connection.ExecuteAsync(query, parameters);
+        }
+
     }
 }
