ef5a5e3fd2088cc41833559a2aa0aa2886554f0a
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-19 12:05:02 +0330

    Refactor  IPostRepository Implement GetAllPosts method in PostRepository

--- Domain/Repositories/IPostRepository.cs
+++ Domain/Repositories/IPostRepository.cs
@@ -4,15 +4,15 @@ namespace Domain.Repositories
 {
     public interface IPostRepository
     {
-        Task<IEnumerable<Post>> GetAllPosts();
+        Task<IEnumerable<Post>> GetAllPosts(int userId);
 
-        Task<Post> GetPostById(int id);
+        Task<Post> GetPostById(int userId, int postId);
 
-        Task CreatePost(Post post);
+        Task CreatePost(int userId, Post post);
 
-        Task DeletePost(int id);
+        Task DeletePost(int userId, int postId);
 
-        Task UpdatePost(int id, Post post);
+        Task UpdatePost(int userId, int postId, Post post);
 
     }
 }
---
+++ Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,29 @@
+﻿
+using Dapper;
+using Domain.Entities;
+using Domain.Repositories;
+
+namespace Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+
+        private readonly DapperContext _dapperContext;
+
+        public PostRepository(DapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+
+
+        public async Task<IEnumerable<Post>> GetAllPosts(int userId)
+        {
+            var query = "SELECT * FROM Posts LEFT JOIN Users ON Posts.UserId = Users.Id";
+
+            var posts = await _dapperContext.Connection.QueryAsync<Post>(query);
+
+            return posts;
+        }
+
+    }
+}
