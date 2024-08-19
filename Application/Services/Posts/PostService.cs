10bfca2239db9890bc4e5880b9f48b13d80dea71
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-19 17:25:30 +0330

    Create PostService class Implement GetAllPosts method

---
+++ Application/Services/Posts/PostService.cs
@@ -0,0 +1,32 @@
+﻿
+using Application.Dtos;
+using Domain.Repositories;
+
+namespace Application.Services.Posts
+{
+    public class PostService : IPostService
+    {
+
+        private readonly IPostRepository _postRepository;
+
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<IEnumerable<PostDto>> GetAllPosts(int userId)
+        {
+            var posts = await _postRepository.GetAllPosts(userId);
+
+            var result = posts.Select(post => new PostDto
+            {
+                Caption = post.Caption,
+                ImageUrl = post.ImageUrl,
+                CreationTime = post.CreationTime,
+            });
+
+            return result;
+        }
+
+    }
+}
