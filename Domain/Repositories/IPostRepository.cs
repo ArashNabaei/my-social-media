1d23251129787450b3fa63d66a864b183dc44feb
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-19 11:49:23 +0330

    Create IPostRepository

---
+++ Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetAllPosts();
+
+        Task<Post> GetPostById(int id);
+
+        Task CreatePost(Post post);
+
+        Task DeletePost(int id);
+
+        Task UpdatePost(int id, Post post);
+
+    }
+}
