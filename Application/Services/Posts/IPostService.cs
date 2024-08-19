779e73015e52c5e5357f80070406570b2d194a61
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-19 17:19:53 +0330

    Add IPostService

---
+++ Application/Services/Posts/IPostService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Dtos;
+
+namespace Application.Services.Posts
+{
+    public interface IPostService
+    {
+
+        Task<IEnumerable<PostDto>> GetAllPosts(int userId);
+
+        Task<PostDto> GetPostById(int userId, int postId);
+
+        Task CreatePost(int userId, PostDto post);
+
+        Task DeletePost(int userId, int postId);
+
+        Task UpdatePost(int userId, int postId, PostDto post);
+    }
+}
