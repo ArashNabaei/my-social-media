35cd763d6d7e5d37e9f09e518ec2a8724a9be2c5
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-19 17:13:40 +0330

    Create PostDto object

---
+++ Application/Dtos/PostDto.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Application.Dtos
+{
+    public class PostDto
+    {
+
+        public string ImageUrl { get; set; }
+
+        public string Caption { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+
+    }
+}
