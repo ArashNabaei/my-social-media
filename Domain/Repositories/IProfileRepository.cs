4410497d731b4d5f4a75a4fb3bae894ad1a7696e
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-22 12:21:13 +0330

    Delete old methods from profile repository

--- Domain/Repositories/IProfileRepository.cs
+++ Domain/Repositories/IProfileRepository.cs
@@ -1,41 +1,11 @@
-﻿
-using Domain.Entities;
+﻿using Domain.Entities;
 
 namespace Domain.Repositories
 {
     public interface IProfileRepository
     {
-        Task<string> GetFirstName(int id);
-
-        Task<string> GetLastName(int id);
-
-        Task<string> GetBio(int id);
-
-        Task<string> GetEmail(int id);
-
-        Task<string> GetPhoneNumber(int id);
-
-        Task<string> GetImageUrl(int id);
-
-        Task<DateTime> GetDateOfBirth(int id);
-
-        Task UpdateFirstName(int id, string firstName);
-
-        Task UpdateLastName(int id, string lastName);
-
-        Task UpdateBio(int id, string bio);
-
-        Task UpdateEmail(int id, string email);
-
-        Task UpdatePhoneNumber(int id, string phoneNumber);
-
-        Task UpdateImageUrl(int id, string imageUrl);
-
-        Task UpdateDateOfBirth(int id, DateTime dateOfBirth);
-
         Task<User> GetProfile(int id);
 
         Task UpdateProfile(int id, User user);
-
     }
 }
