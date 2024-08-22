a153a6c9b5f535fd39831b269db7c0606c30e983
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-22 12:19:35 +0330

    Remove unused methods from ProfileService

--- Application/Services/Profiles/IProfileService.cs
+++ Application/Services/Profiles/IProfileService.cs
@@ -4,38 +4,8 @@ namespace Application.Services.Profiles
 {
     public interface IProfileService
     {
-
-        Task<string> GetBio(int id);
-
-        Task<DateTime> GetDateOfBirth(int id);
-
-        Task<string> GetEmail(int id);
-
-        Task<string> GetFirstName(int id);
-
-        Task<string> GetLastName(int id);
-
-        Task<string> GetImageUrl(int id);
-
-        Task<string> GetPhoneNumber(int id);
-
-        Task UpdateBio(int id, string bio);
-
-        Task UpdateDateOfBirth(int id, DateTime dateOfBirth);
-
-        Task UpdateEmail(int id, string email);
-
-        Task UpdateFirstName(int id, string firstName);
-
-        Task UpdateImageUrl(int id, string imageUrl);
-
-        Task UpdatePhoneNumber(int id, string phoneNumber);
-
-        Task UpdateLastName(int id, string lastName);
-
         Task<User> GetProfile(int id);
 
         Task UpdateProfile(int id, User user);
-
     }
 }
