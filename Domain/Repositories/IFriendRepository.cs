31bfa7b3e1f66989fb35a0611535888d2a71726f
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-24 12:16:57 +0330

    Create IFriendRepository

---
+++ Domain/Repositories/IFriendRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IFriendRepository
+    {
+        Task<IEnumerable<User>> GetAllFriends();
+
+        Task<IEnumerable<User>> GetAllFollowers();
+        
+        Task<IEnumerable<User>> GetAllFollowings();
+
+        Task<User> GetFollowerById(int id);
+
+        Task<User> GetFollowingById(int id);
+
+        Task RemoveFollower(int id);
+
+        Task RemoveFollowing(int id);
+
+        Task Follow(int id);
+    }
+}
