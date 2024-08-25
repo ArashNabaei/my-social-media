80c12d537fab9c6bb4d2690eb96d2071000eba9c
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-25 09:23:51 +0330

    Create a Dto as UserDto

---
+++ Application/Dtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Application.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Bio { get; set; }
+
+        public string ImageUrl {  get; set; }
+    }
+}
