bf694e58338eca5883320ce2243c7e7086acc1c4
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-25 10:04:43 +0330

    Make a controller for Follow APIs Implement GetAllFriends API

---
+++ my-social-media/Controllers/FollowController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Follows;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_social_media.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class FollowController : BaseController
+    {
+        private readonly IFollowService _followService;
+
+        public FollowController(IFollowService followService)
+        {
+            _followService = followService;
+        }
+
+        [HttpGet("GetAllfriends")]
+        public async Task<IActionResult> GetAllfriends()
+        {
+            var friends = await _followService.GetAllFriends(UserId);
+
+            return Ok(friends);
+        }
+
+    }
+}
