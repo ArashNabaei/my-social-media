342cf4efa910928f0c8e781ef9787214717fc448
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-25 09:33:52 +0330

    Implement GetAllFriends in FollowService

---
+++ Application/Services/Follows/FollowService.cs
@@ -0,0 +1,32 @@
+﻿using Application.Dtos;
+using Domain.Repositories;
+
+namespace Application.Services.Follows
+{
+    public class FollowService : IFollowService
+    {
+        private readonly IFollowRepository _followRepository;
+
+        public FollowService(IFollowRepository followRepository)
+        {
+            _followRepository = followRepository;
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllFriends(int userId) 
+        {
+            var friends = await _followRepository.GetAllFriends(userId);
+
+            var result = friends.Select(friend => new UserDto
+            {
+                Id = friend.Id,
+                FirstName = friend.FirstName,
+                LastName = friend.LastName,
+                Bio = friend.Bio,
+                ImageUrl = friend.ImageUrl,
+            });
+
+            return result;
+        }
+
+    }
+}
