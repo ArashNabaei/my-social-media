40d5b0a9009ec603866b628de984b4a67faa77f4
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-25 09:53:07 +0330

    Create and make some exceptions for FollowService as FollowException

---
+++ Shared/Exceptions/Follows/FollowException.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.Exceptions.Follows
+{
+    public class FollowException : BaseException
+    {
+        public FollowException(int code, string message) : base(code, message)
+        {
+        }
+
+        public static FollowException FollowerNotFound()
+        {
+            return new FollowException(4001, "Follower was not found.");
+        }
+
+        public static FollowException FollowingNotFound()
+        {
+            return new FollowException(4002, "Following was not found.");
+        }
+
+    }
+}
