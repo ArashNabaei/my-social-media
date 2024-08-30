e9df12457d23028ca1e142cdc678518598ec1055
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-30 18:57:09 +0330

    Create new controller as ChatController Add an end point for sending message in ChatController

---
+++ my-social-media/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services.Chats;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_social_media.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ChatController : BaseController
+    {
+        private readonly IChatService _chatService;
+
+        public ChatController(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpPost("SendMessage")]
+        public async Task<IActionResult> SendMessage(int receiverId, [FromBody] string message)
+        {
+            await _chatService.SendMessage(UserId, receiverId, message);
+
+            return Ok("Message sent successfully.");
+        }
+
+    }
+}
