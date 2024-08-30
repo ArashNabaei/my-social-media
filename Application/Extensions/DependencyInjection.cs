e630f60015f09cc1f8a358d23364359e25f1f41f
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-30 19:04:01 +0330

    Add chat services to Ioc container

--- Application/Extensions/DependencyInjection.cs
+++ Application/Extensions/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Services.Accounts;
+using Application.Services.Chats;
 using Application.Services.Follows;
 using Application.Services.Posts;
 using Application.Services.Profiles;
@@ -18,6 +19,7 @@ namespace Application.Extensions
             services.AddScoped<IProfileService, ProfileService>();
             services.AddScoped<IPostService, PostService>();
             services.AddScoped<IFollowService, FollowService>();
+            services.AddScoped<IChatService, ChatService>();
 
             return services;
         }
