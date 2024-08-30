84dc3e54a3b274f0711c388343475307435d8da6
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-30 17:36:32 +0330

    Create an interface for chat as IChatRepository

---
+++ Domain/Repositories/IChatRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IChatRepository
+    {
+        Task SendMessage(int senderId, int receiverId,  string message);
+
+        Task<IEnumerable<Message>> GetAllMessages(int userId, int id);
+
+        Task DeleteMessage(int userId, int messageId);
+
+        Task UpdateMessage(int userId, int messageId, string message);
+    }
+}
