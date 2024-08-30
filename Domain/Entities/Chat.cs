28384dbcef9a698a522c472cc05acccf806c8014
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-30 17:34:37 +0330

    Create an object as Chat

---
+++ Domain/Entities/Chat.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+
+        public int SenderId { get; set; }
+
+        public int ReceiverId { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
