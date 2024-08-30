93fdde18082cf67b19cfee14c150526813e77482
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-30 18:34:51 +0330

    Implement SendMessage method in ChatService

---
+++ Application/Services/Chats/ChatService.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Repositories;
+
+namespace Application.Services.Chats
+{
+    public class ChatService : IChatService
+    {
+
+        private readonly IChatRepository _chatRepository;
+
+        public ChatService(IChatRepository chatRepository)
+        {
+            _chatRepository = chatRepository;
+        }
+
+        public async Task SendMessage(int senderId, int receiverId, string message)
+        {
+            await _chatRepository.SendMessage(senderId, receiverId, message);
+        }
+
+    }
+}
