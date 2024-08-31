9fbe4ee6c768d8cbcd280526543129321e77af42
ArashNabaei <Arash_ANB@yahoo.com>
2024-08-31 17:19:21 +0330

    Create a middleware for logging as LoggingMiddleware Implement Invoke method

---
+++ my-social-media/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace my_social_media.Middlewares
+{
+    public class LoggingMiddleware : IMiddleware
+    {
+        private readonly ILogger _logger;
+
+        public LoggingMiddleware(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                _logger.LogInformation("Handling request: {Method} {Path}", context.Request.Method, context.Request.Path);
+
+                await next(context);
+
+                _logger.LogInformation("Finished handling request: {Method} {Path}", context.Request.Method, context.Request.Path);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An exception occurred while handling the request");
+            }
+        }
+    }
+}
