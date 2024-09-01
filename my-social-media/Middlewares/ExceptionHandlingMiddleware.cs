9128fcdb3bf2a832291d65cc1555ae5b25efac92
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-01 17:06:57 +0330

    Create ExceptionHandlingMiddleware

---
+++ my-social-media/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace my_social_media.Middlewares
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An unexpected error occurred.",
+                Detailed = exception.Message
+            };
+
+            return context.Response.WriteAsJsonAsync(response);
+        }
+
+    }
+}
