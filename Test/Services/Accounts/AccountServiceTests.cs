b784d2b554308e3d6dd73d0813bffa9e3b57c9e3
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-10 12:23:16 +0330

    Add reference from test to application project Setup mock in AccountServiceTest Inject mocks to constructor

--- Test/Services/Accounts/AccountServiceTests.cs
+++ Test/Services/Accounts/AccountServiceTests.cs
@@ -1,6 +1,38 @@
-﻿namespace Test.Services.Accounts
+﻿using Application.Services.Accounts;
+using Microsoft.Extensions.Configuration;
+using Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Test.Services.Accounts
 {
     public class AccountServiceTests
     {
+
+        private readonly Mock<IAccountRepository> _accountRepository;
+
+        private readonly Mock<IConfiguration> _configuration;
+
+        private readonly Mock<ILogger<AccountService>> _logger;
+
+        private readonly IAccountService _accountService;
+
+        public AccountServiceTests()
+        {
+            _accountRepository = new Mock<IAccountRepository>();
+            _configuration = new Mock<IConfiguration>();
+            _logger = new Mock<ILogger<AccountService>>();
+
+            _configuration.Setup(config => config.GetValue<string>("Jwt:Key")).Returns("here_is_my_secret_key_that_is_used_for_Jwt");
+            _configuration.Setup(config => config.GetValue<double>("Jwt:ExpiryMinutes")).Returns(30);
+
+            _accountService = new AccountService(
+                _accountRepository.Object,
+                _configuration.Object,
+                _logger.Object);
+        }
+
+
+
     }
 }
