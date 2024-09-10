5364411308eaa85357fd1f3b99b6e20362c58f12
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-10 19:51:26 +0330

    Create AccountMocks class Add some mock data to AccountMocks

---
+++ Test/Mocks/AccountMocks.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+
+namespace Test.Mocks
+{
+    public static class AccountMocks
+    {
+
+        public static User ValidUser()
+        {
+            return new User
+            {
+                Id = 1,
+                Username = "ValidUsername",
+                Password = "ValidPassword"
+            };
+        }
+
+        public static User InValidUser()
+        {
+            return new User
+            {
+                Username = "InValidUsername",
+                Password = "InValidPassword"
+            };
+        }
+
+        public static User ExistingUser()
+        {
+            return new User
+            {
+                Id = 1,
+                Username = "ExistingUsername",
+                Password = "ExistingPassword"
+            };
+        }
+
+    }
+}
