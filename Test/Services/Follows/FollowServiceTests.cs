5d013cbb848c8b64e37430fe367476656805cedb
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-11 18:25:57 +0330

    Create a mock file for data as FollowMocks Add a test for GetAllFriends when friends exist

--- Test/Services/Follows/FollowServiceTests.cs
+++ Test/Services/Follows/FollowServiceTests.cs
@@ -1,7 +1,11 @@
-﻿using Application.Services.Follows;
+﻿using Application.Dtos;
+using Application.Services.Follows;
+using Domain.Entities;
 using Domain.Repositories;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Test.Mocks;
+using Xunit;
 
 namespace Test.Services.Follows
 {
@@ -26,5 +30,38 @@ namespace Test.Services.Follows
                 );
         }
 
+        [Fact]
+        public async Task GetAllFriends_WhenFriendsExist_ShouldReturnsFriends()
+        {
+            int userId = 2;
+
+            var friends = new List<User>();
+            friends.Add(FollowMocks.ValidUser());
+
+            _followRepository.Setup(r => r.GetAllFriends(userId))
+                .ReturnsAsync(friends);
+
+            var expectedFriends = ConvertUserToUserDto(friends);
+
+            var result = await _followService.GetAllFriends(userId);
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedFriends.Count(), result.Count());
+            Assert.Equal(expectedFriends.First().Id, result.First().Id);
+            Assert.Equal(expectedFriends.First().FirstName, result.First().FirstName);
+            Assert.Equal(expectedFriends.First().LastName, result.First().LastName);
+        }
+
+        private static IEnumerable<UserDto> ConvertUserToUserDto(List<User> friends)
+        {
+            return friends.Select(f => new UserDto
+            {
+                Id = f.Id,
+                FirstName = f.FirstName,
+                LastName = f.LastName,
+                Bio = f.Bio,
+                ImageUrl = f.ImageUrl,
+            });
+        }
     }
 }
