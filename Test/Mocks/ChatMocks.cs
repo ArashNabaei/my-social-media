54da3aed6a7176fc6455178cca712694931cafc4
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-11 17:06:44 +0330

    Create ChatMocks file Add a new test to ChatServiceTests for get all messages

---
+++ Test/Mocks/ChatMocks.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Test.Mocks
+{
+    public static class ChatMocks
+    {
+
+        public static Message ValidMessage()
+        {
+            return new Message
+            {
+                Id = 1, SenderId = 1, ReceiverId = 2, Content = "valid message"
+            };
+        }
+
+    }
+}
--- Test/Services/Chats/ChatServiceTests.cs
+++ Test/Services/Chats/ChatServiceTests.cs
@@ -1,7 +1,11 @@
 ﻿using Application.Services.Chats;
+using Domain.Entities;
 using Domain.Repositories;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Shared.Exceptions.Chats;
+using Test.Mocks;
+using Xunit;
 
 namespace Test.Services.Chats
 {
@@ -22,6 +26,23 @@ namespace Test.Services.Chats
             _chatService = new ChatService(
                 _chatRepository.Object,
                 _logger.Object);
+        }
+
+        [Fact]
+        public async Task GetAllMessages_WhenMessagesExist_ShouldReturnsMessages()
+        {
+            int userId = 1;
+            int chatId = 2;
+
+            var messages = new List<Message>();
+            messages.Add(ChatMocks.ValidMessage());
+
+            _chatRepository.Setup(r => r.GetAllMessages(userId, chatId))
+                           .ReturnsAsync(messages);
+
+            var result = await _chatService.GetAllMessages(userId, chatId);
+
+            Assert.Equal(messages, result);
         }
 
     }
