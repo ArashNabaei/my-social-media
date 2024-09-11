f9266027e4151ccf85e34615d60c9fa47e9c3fdc
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-11 17:52:43 +0330

    Create new directory for test follow service methods Add FollowServiceTests class

---
+++ Test/Services/Follows/FollowServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Application.Services.Follows;
+using Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Test.Services.Follows
+{
+    public class FollowServiceTests
+    {
+
+        private readonly Mock<IFollowRepository> _followRepository;
+
+        private readonly Mock<ILogger<FollowService>> _logger;
+
+        private readonly IFollowService _followService;
+
+        public FollowServiceTests()
+        {
+            _followRepository = new Mock<IFollowRepository>();
+
+            _logger = new Mock<ILogger<FollowService>>();
+
+            _followService = new FollowService(
+                _followRepository.Object,
+                _logger.Object
+                );
+        }
+
+    }
+}
