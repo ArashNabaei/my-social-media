22778e13428b3a749b2d211743f15578f535c3cd
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-11 11:44:19 +0330

    Add new directory for chat service in Test project Create ChatServiceTest class

---
+++ Test/Services/Chats/ChatServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Chats;
+using Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Test.Services.Chats
+{
+    public class ChatServiceTest
+    {
+
+        private readonly Mock<IChatRepository> _chatRepository;
+
+        private readonly Mock<ILogger<ChatService>> _logger;
+
+        private readonly IChatService _chatService;
+
+        public ChatServiceTest()
+        {
+            _chatRepository = new Mock<IChatRepository> ();
+            _logger = new Mock<ILogger<ChatService>> ();
+
+            _chatService = new ChatService(
+                _chatRepository.Object,
+                _logger.Object);
+        }
+
+    }
+}
