9662eb7a2e87fa1c490a37c3a96aeeb00882d734
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-14 19:23:29 +0330

    Create new directory for tests of Profile service Add new file as ProfileServiceTest

---
+++ Test/Services/Profiles/ProfileServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Application.Services.Profiles;
+using Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Test.Services.Profiles
+{
+    public class ProfileServiceTest
+    {
+
+        private readonly Mock<IProfileRepository> _profileRepository;
+
+        private readonly Mock<ILogger<ProfileService>> _logger;
+
+        private readonly IProfileService _profileService;
+
+        public ProfileServiceTest()
+        {
+            _profileRepository = new Mock<IProfileRepository>();
+
+            _logger = new Mock<ILogger<ProfileService>>();
+
+            _profileService = new ProfileService(
+                _profileRepository.Object,
+                _logger.Object
+                );
+        }
+
+    }
+}
