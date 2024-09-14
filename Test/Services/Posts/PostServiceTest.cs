fa48d4ba1ad6aac4e1a0bad4ce6ec610aa73e5bc
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-14 11:59:58 +0330

    Create new directory for tests of post service Add new file as PostServiceTest

---
+++ Test/Services/Posts/PostServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Application.Services.Posts;
+using Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Test.Services.Posts
+{
+    public class PostServiceTest
+    {
+
+        private readonly Mock<IPostRepository> _postRepository;
+
+        private readonly Mock<ILogger<PostService>> _logger;
+
+        private readonly PostService _postService;
+
+        public PostServiceTest()
+        {
+            _postRepository = new Mock<IPostRepository>();
+
+            _logger = new Mock<ILogger<PostService>>();
+
+            _postService = new PostService(
+                _postRepository.Object, 
+                _logger.Object
+                );
+        }
+
+    }
+}
