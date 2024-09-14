a10441ec882a371b8537b747fdad02c5b53fc704
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-14 12:07:38 +0330

    Create PostMock class Add ValidPost as a data to PostMocks

---
+++ Test/Mocks/PostMocks.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dtos;
+
+namespace Test.Mocks
+{
+    public static class PostMocks
+    {
+
+        public static PostDto ValidPost()
+        {
+            return new PostDto
+            {
+                Id = 1,
+                ImageUrl = "imageUrl", 
+                Caption = "caption", 
+                CreatedAt = DateTime.UtcNow,
+            };
+        }
+
+    }
+}
