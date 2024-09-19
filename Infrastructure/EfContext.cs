bd76c4259e947d0f56b331458522f23ba88672aa
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-19 08:21:03 +0330

    Implement EfContext class Add tables

--- Infrastructure/EfContext.cs
+++ Infrastructure/EfContext.cs
@@ -1,7 +1,31 @@
-﻿namespace Infrastructure
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
 {
-    public class EFContext
+    public class EfContext : DbContext
     {
+        
+        public EfContext(DbContextOptions<EfContext> options) : base(options)
+        {
+            OnModelCreating(new ModelBuilder());
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+        
+        public DbSet<Like> Likes { get; set; }
+        
+        public DbSet<Post> Posts { get; set; }
+        
+        public DbSet<Message> Messages { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
 
     }
 }
