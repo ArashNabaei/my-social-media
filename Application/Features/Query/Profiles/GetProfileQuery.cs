323cb89b6999d095d42474d247c653600bc277c5
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-25 22:04:21 +0330

    Create Features directory for implemening CQRS Add GetProfileQuery class

---
+++ Application/Features/Query/Profiles/GetProfileQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Application.Features.Query.Profiles
+{
+    public record GetProfileQuery(int Id, string FirstName, 
+        string LastName, string Username, 
+        string Password, string Bio, 
+        string Email, string PhoneNumber, 
+        string ImageUrl, DateTime DateOfBirth) : IRequest
+    {
+    }
+}
