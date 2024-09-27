a323f82249dcffaaf6bae8328124a2ccf75ae8b3
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-27 18:06:35 +0330

    Implement handler for search user by name

---
+++ Application/Features/Query/Chats/SearchUserByNameQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Dtos;
+using Application.Services.Chats;
+using MediatR;
+
+namespace Application.Features.Query.Chats
+{
+    public class SearchUserByNameQueryHandler : IRequestHandler<SearchUserByNameQuery, IEnumerable<UserDto>>
+    {
+        
+        private readonly IChatService _chatService;
+
+        public SearchUserByNameQueryHandler(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public async Task<IEnumerable<UserDto>> Handle(SearchUserByNameQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _chatService.SearchUserByName(request.UserId, request.Pattern);
+
+            return users;
+        }
+
+    }
+}
