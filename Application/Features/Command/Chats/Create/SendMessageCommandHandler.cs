4e22e5ee81273d8b9040bf874295722cadf9175e
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-27 18:26:54 +0330

    Implement handler for sending message

---
+++ Application/Features/Command/Chats/Create/SendMessageCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services.Chats;
+using MediatR;
+
+namespace Application.Features.Command.Chats.Create
+{
+    public class SendMessageCommandHandler : IRequestHandler<SendMessageCommand>
+    {
+
+        private readonly IChatService _chatService;
+
+        public SendMessageCommandHandler(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public async Task Handle(SendMessageCommand request, CancellationToken cancellationToken)
+        {
+            await _chatService.SendMessage(request.SenderId, request.ReceiverId, request.Message);
+        }
+
+    }
+}
